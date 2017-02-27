24f0c3798fbaed2868978f3a45bea5dba23bd97c
Roshan Thapaliya <gorkhali@Roshans-MacBook-Pro.local>
2017-02-27 17:22:41 -0500

    Defining data structures

--- unicity.c
+++ unicity.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include <math.h>
  
+
+struct Subset {
+	char subset_val[50];
+	struct Subset * next;
+	struct Unique * unique_next;
+} subset;
+
+
+struct Unique {
+	char unique_val[50];
+	struct Unique * next;
+} unique;
+
+// a = '''11111 1 1 1 1
+// 12222 1 2 2 2 
+// 13333 2 1 3 3
+// 14444 2 2 1 4
+// 15555 3 1 2 1
+// 16666 3 2 3 2
+// 17777 1 3 1 3
+// 18888 2 3 2 4
+// 19999 3 3 3 1
+// 20000 1 1 1 2
+// 21111 2 2 2 3
+// 22222 3 3 3 4
+// 22223 4 1 1 1
+// 22224 4 2 2 2
+// 22225 4 3 3 3
+// 22226 4 4 4 4'''
+
 void printPowerSet(char *set, int set_size)
 {
     /*set_size of power set of a set with set_size
@@ -27,7 +57,6 @@ int main()
 {
     char set[] = {'a','b','c'};
     printPowerSet(set, 3);
- 
     getchar();
     return 0;
 }
