88370a102c21aef2bcbf924d8b240e91b088f773
Roshan Thapaliya <gorkhali@Roshans-MacBook-Pro.local>
2017-03-07 09:24:24 -0500

    Ready to add to hashmap

--- unicity.c
+++ unicity.c
@@ -8,17 +8,19 @@
 
 void printPowerSet(char *set, int set_size)
 {
+
+    // initiate hashmap
+
+    
+
     /*set_size of power set of a set with set_size
       n is (2**n -1)*/
     unsigned int pow_set_size = pow(2, set_size);
     int counter, j;
- 
-
-    
     /*Run from counter 000..0 to 111..1*/
     for(counter = 0; counter < pow_set_size; counter++)
     {
-      char subset_val[4];
+      char subset_val[4+1];
 
       int index = 0;
       for(j = 0; j < set_size; j++)
@@ -29,31 +31,38 @@ void printPowerSet(char *set, int set_size)
             //printf("%c", set[j]);
             subset_val[index] = set[j];
             index ++;
+            //printf("%d\n",index);
             // each index coming over here
           }
-
-
        }
 
+       
        // Now based on index add values after reading from file
+       
+       // Add null terminating character at the end
+       subset_val[index] = '\0';
+       printf("%s\n",subset_val);
+
+
        for (int i=0; i< index;i++){
         printf("%c",subset_val[i]);
        }
 
-
+       //reset subset_val and index
+       subset_val[0] = '\0';
        index = 0;
+       
+       
        printf("\n");
-
-    }
-
     
+    }
 
 }
  
 /*Driver program to test printPowerSet*/
 int main()
 {
-  static const char filename[] = "data.txt";
+ static const char filename[] = "data.txt";
    FILE *file = fopen ( filename, "r" );
    if ( file != NULL )
    {
@@ -62,9 +71,7 @@ int main()
       while ( fgets ( line, sizeof line, file ) != NULL ) /* read a line */
       {
         //fputs ( line, stdout ); /* write the line */
-        
         p = strtok(line," ");
-
         // from each line now we read word by word
         while(p != NULL)
         {
@@ -72,7 +79,6 @@ int main()
           p = strtok(NULL," ");
         }
         // main logic goes here get the different combinations and add to hashmap
-
       }
       fclose ( file );
    }
