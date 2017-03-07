b1b47d5b2f78ca306b4f178b30b76ec530c117f6
Roshan Thapaliya <gorkhali@Roshans-MacBook-Pro.local>
2017-03-07 06:56:05 -0500

    Reading line by line completed

--- unicity.c
+++ unicity.c
@@ -68,8 +68,23 @@ void printPowerSet(char *set, int set_size)
 /*Driver program to test printPowerSet*/
 int main()
 {
-    char set[] = {'a','b','c'};
-    printPowerSet(set, 3);
-    getchar();
-    return 0;
+  static const char filename[] = "data.txt";
+   FILE *file = fopen ( filename, "r" );
+   if ( file != NULL )
+   {
+      char line [ 128 ]; /* or other suitable maximum line size */
+      while ( fgets ( line, sizeof line, file ) != NULL ) /* read a line */
+      {
+         fputs ( line, stdout ); /* write the line */
+      }
+      fclose ( file );
+   }
+   else
+   {
+      perror ( filename ); /* why didn't the file open? */
+   }
+
+  char set[] = {'a','b','c'};
+  printPowerSet(set, 3);
+  return 0;
 }
